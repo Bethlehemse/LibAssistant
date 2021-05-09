fedab0446847c2b90e1369acaf0726fc78d93a10
Akos Hofmeister <akos.hofmeister@calmarkr.com>
2021-05-09 17:38:53 +0200

    Add books structure css

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './index.css';
 
-import Login from "./components/login.component";
-import SignUp from "./components/signup.component";
-import Books from "./components/books.component";
+import Login from './components/auth/login.component';
+import SignUp from './components/auth/signup.component';
+import Books from './components/books.component';
+import NavBar from './components/navbar.component';
 
 function App() {
-  return (<Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>Lib Assistant</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-            <Route path="/books" component={Books} />
-          </Switch>
-        </div>
+  return (
+    <Router>
+      <div className='App'>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={Books} />
+          <Route path='/sign-in' component={Login} />
+          <Route path='/sign-up' component={SignUp} />
+          <Route path='/books' component={Books} />
+        </Switch>
       </div>
-    </div></Router>
+    </Router>
   );
 }
 
--- src/components/books.component.js
+++ src/components/books.component.js
@@ -1,5 +1,158 @@
 import React from 'react';
 
 export default () => {
-    return <strong>books</strong>
-}
+  return (
+    <div className='content-wrapper'>
+      <div className='content-inner'>
+        <div>
+          <h3>Books</h3>
+        </div>
+        <div class='books-container'>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <img src='...' class='card-img-top' alt='...' />
+            <div class='card-body'>
+              <h5 class='card-title'>Card title</h5>
+              <p class='card-text'>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href='#' class='btn btn-primary'>
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
